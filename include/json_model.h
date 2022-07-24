781c6d85cd42fee0b7375c796faf7b6b9f2c889c
Ronald Anderson <roonald.anderson017@gmail.com>
2022-07-24 16:49:40 -0300

    Added from and to_json

--- include/json_model.h
+++ include/json_model.h
@@ -1,22 +1,67 @@
 #pragma once
 
-#include "Guest.h"
-#include "Person.h"
+#include "Hotel.h"
 #include <nlohmann/json.hpp>
+#include <iomanip>
 
 using json = nlohmann::json;
+using namespace std;
 
-void to_json(json &j, const Person &p)
+namespace ns
 {
-    j = json();
-    j["name"] = p.name;
-    j["address"] = p.address;
-    j["phone"] = p.phone;
+
+    void to_json(json &j, const ns::Person &p);
+    void from_json(const json &j, ns::Person &p);
+    void to_json(json &j, const ns::Guest &guest);
+    void from_json(const json &j, ns::Guest &guest);
+    void to_json(json &j, const HotelDataCtx &hotelData);
+    void from_json(const json &j, HotelDataCtx &hotelData);
+
 }
 
-void from_json(const json &j, Person &p)
+void ns::to_json(json &j, const ns::Person &p)
+{
+    j = json{
+        {"name", p.name},
+        {"address", p.address},
+        {"phone", p.phone}};
+}
+
+void ns::from_json(const json &j, ns::Person &p)
 {
     j.at("name").get_to(p.name);
     j.at("address").get_to(p.address);
     j.at("phone").get_to(p.phone);
+}
+
+void ns::to_json(json &j, const ns::Guest &guest)
+{
+    j = json{
+        {"person", guest.person},
+        {"room_num", guest.room_num},
+        {"days", guest.days},
+        {"fare", guest.fare}
+    };
+}
+
+void ns::from_json(const json &j, ns::Guest &guest)
+{
+    j.at("person").get_to(guest.person);
+    j.at("room_num").get_to(guest.room_num);
+    j.at("days").get_to(guest.days);
+    j.at("fare").get_to(guest.fare);
+}
+
+void ns::to_json(json &j, const ns::HotelDataCtx &hotelData)
+{
+    j = json{
+        {"guests", {hotelData.listOfGuests}},
+        {"registeredPersons", {hotelData.listOfRegisteredperson}}
+    };
+}
+
+void ns::from_json(const json &j, ns::HotelDataCtx &hotelData)
+{
+    // j.at("guests").get_to(hotelData.listOfGuests);
+    // j.at("registeredPerson").get_to(hotelData.listOfRegisteredperson);
 }
---
+++ src/person.cpp
@@ -0,0 +1,6 @@
+#include <person.h>
+
+ns::Person::Person(string name, string address, string phone)
+        : name(name), address(address), phone(phone)
+{
+}
